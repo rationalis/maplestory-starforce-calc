3d5ad5dd816e847477478107d85bd0e469eed7e1
Jimmy Ye <jiy162@ucsd.edu>
2021-02-06 02:48:15 -0800

    Add stars up to 22, higher probability threshold to improve accuracy

--- src/main.rs
+++ src/main.rs
@@ -7,24 +7,33 @@ use lazy_static::*;
 use priority_queue::PriorityQueue;
 
 type F = U1F63;
+type Q = PriorityQueue<(Star, Meso, bool), F, std::hash::BuildHasherDefault<rustc_hash::FxHasher>>;
 type Meso = i32;
+type Star = u8;
 
 const UNIT: Meso = 100_000;
+const STAR_LIMIT: Star = 22;
+const PROB_COUNT: usize = (STAR_LIMIT - 10) as usize;
 
-const PROBS_F32: [[f32; 4]; 7] = [
+const PROBS_F32: [[f32; 4]; PROB_COUNT] = [
     [ 0.5, 0.5, 0., 0. ],
     [ 0.45, 0., 0.55, 0. ],
     [ 0.4, 0., 0.594, 0.006 ],
     [ 0.35, 0., 0.637, 0.013 ],
     [ 0.3, 0., 0.686, 0.014 ],
-    [ 0.3, 0.679, 0., 0.021 ],
-    [ 0.3, 0., 0.679, 0.021 ]
+    [ 0.3, 0.679, 0., 0.021 ], // 15
+    [ 0.3, 0., 0.679, 0.021 ], // 16
+    [ 0.3, 0., 0.679, 0.021 ], // 17
+    [ 0.3, 0., 0.672, 0.028 ], // 18
+    [ 0.3, 0., 0.672, 0.028 ], // 19
+    [ 0.3, 0.63, 0., 0.07 ], // 20
+    [ 0.3, 0., 0.63, 0.07 ], // 21
 ];
 
 lazy_static! {
-    static ref PROBS: [[F; 4]; 7] = {
-        let mut probs: [[F; 4]; 7] = Default::default();
-        for i in 0..7 {
+    static ref PROBS: [[F; 4]; PROB_COUNT] = {
+        let mut probs: [[F; 4]; PROB_COUNT] = Default::default();
+        for i in 0..12 {
             for j in 0..4 {
                 probs[i][j] = F::from_num(PROBS_F32[i][j]);
             }
@@ -33,20 +42,31 @@ lazy_static! {
     };
     static ref LEVEL: i32 = 160;
     // TODO: add level-dependent cost tables
-    static ref COST: [Meso; 17] = {
-        let mut cost = [0; 17];
+    static ref COST: [Meso; STAR_LIMIT as usize] = {
+        let mut cost = [0; STAR_LIMIT as usize];
 
-        for i in 10..17 {
+        for i in 10..22 {
             let i = i as usize;
-            cost[i] = crate::cost(i as i32, *LEVEL);
+            cost[i] = crate::cost(i as u8, *LEVEL);
         }
 
         cost
     };
 }
 
-fn round(mesos: i32, unit: i32) -> i32 {
+fn round(mesos: Meso, unit: i32) -> Meso {
     (mesos + (unit / 2)) / unit * unit
+}
+
+fn round_bucket(mesos: Meso) -> Meso {
+    let c = mesos;
+    if c > 10000 {
+        round(c, 100)
+    } else if c > 1000 {
+        round(c, 10)
+    } else {
+        c
+    }
 }
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
@@ -68,13 +88,7 @@ impl Distribution {
         let mut small_dist = BTreeMap::new();
 
         for &(c, p) in full_dist.iter() {
-            let c = if c > 10000 {
-                round(c, 100)
-            } else if c > 1000 {
-                round(c, 10)
-            } else {
-                c
-            };
+            let c = round_bucket(c);
             if let Some(&old_prob) = small_dist.get(&c) {
                 small_dist.insert(c, old_prob + p);
             } else {
@@ -82,15 +96,6 @@ impl Distribution {
             }
         }
 
-        // let mut dist = Vec::new();
-        // let mut accumulated_prob = 0f64;
-        // for (&c, &p) in small_dist.iter() {
-        //     dist.push((c, p));
-        //     accumulated_prob += p.to_num::<f64>();
-        //     if accumulated_prob > 0.999 {
-        //         break;
-        //     }
-        // }
         let dist = small_dist.into_iter().collect();
 
         Self {dist, full_dist}
@@ -99,19 +104,19 @@ impl Distribution {
 
 #[derive(Default)]
 struct TransitionTable {
-    dists: HashMap<(i32, bool), BTreeMap<i32, Distribution>>
+    dists: HashMap<(Star, bool), BTreeMap<Star, Distribution>>
 }
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
 struct State {
     prob: F,
-    star: i32,
+    star: Star,
     spent: Meso,
     downed: bool,
 }
 
 impl State {
-    fn new(star: i32, downed: bool) -> Self {
+    fn new(star: Star, downed: bool) -> Self {
         Self {
             prob: F::from_num(1.),
             star,
@@ -138,11 +143,7 @@ impl State {
         if self.downed && t == Transition::Down {
             spent += COST[(self.star - 1) as usize];
         }
-        if spent > 50000 {
-            spent = round(spent, 100);
-        } else if spent > 5000 {
-            spent = round(spent, 10);
-        }
+        let spent = round_bucket(spent);
         Some(Self {
             prob: self.prob * p,
             star: next_star,
@@ -185,19 +186,19 @@ impl State {
         }
     }
 
-    fn key(&self) -> (i32, Meso, bool) {
+    fn key(&self) -> (Star, Meso, bool) {
         (self.star, self.spent, self.downed)
     }
 }
 
 #[derive(Default)]
 struct States {
-    all_states: PriorityQueue<(i32, Meso, bool), F, std::hash::BuildHasherDefault<rustc_hash::FxHasher>>,
+    all_states: Q,
     successes: HashMap<Meso, F>,
     total_prob: F,
-    target: i32,
-    push_counter: i32,
-    merge_counter: i32,
+    target: Star,
+    push_counter: i64,
+    merge_counter: i64,
     min_prob: F,
 }
 
@@ -233,12 +234,14 @@ impl States {
     }
 }
 
-fn cost(star: i32, level: i32) -> i32 {
+fn cost(star: Star, level: i32) -> i32 {
     let level_factor: f32 = level.pow(3) as f32;
     let star_factor: f32 = ((star+1) as f32).powf(2.7);
     let denom: f32 = match star {
         n if 10 <= n && n <= 14 => 400,
         n if 15 <= n && n <= 17 => 120,
+        n if 18 <= n && n <= 19 => 110,
+        n if 20 <= n && n <= 22 => 100,
         _ => panic!(),
     } as f32;
     let cost = 1000f32 + (level_factor as f32) * star_factor / denom;
@@ -249,9 +252,9 @@ fn cost(star: i32, level: i32) -> i32 {
 
 fn calculate2(level: i32) {
     let mut table = TransitionTable::default();
-    for target in 11..18 {
+    for target in 11..STAR_LIMIT {
         for start in (10..target).rev() {
-            for &downed in &[true, false] {
+            for &downed in &[false, true] {
                 println!("Starting {} downed: {} -> {}", start, downed, target);
                 let states = calculate(State::new(start, downed), target, level, &mut table);
                 let dist = Distribution::new(states.successes.into_iter().collect());
@@ -267,38 +270,41 @@ fn calculate2(level: i32) {
     }
 }
 
-fn calculate(start: State, target: i32, level: i32, table: &mut TransitionTable) -> States {
+fn calculate(start: State, target: Star, level: i32, table: &mut TransitionTable) -> States {
     let mut states = States {
         target,
         min_prob: F::from_num(1.),
         ..Default::default()};
     let mut last_total_prob = F::from_num(1.0);
-    let threshold = 1e-3;
+    let threshold = if target <= 17 { 1e-5 } else { 1e-4 };
     states.push(start);
     while states.total_prob > threshold {
         states.pop().add_transitions(&mut states, table);
-        let mut progressed = false;
-        for magnitude in 1.. {
-            let oom = 0.1f32.powf(magnitude as f32);
-            if last_total_prob > oom {
-                progressed = states.total_prob <= last_total_prob - F::from_num(oom);
-                break;
+        if true {
+        // if cfg!(debug_assertions) {
+            let mut progressed = false;
+            for magnitude in 1.. {
+                let oom = 0.1f32.powf(magnitude as f32);
+                if last_total_prob > oom {
+                    progressed = states.total_prob <= last_total_prob - F::from_num(oom);
+                    break;
+                }
+                if oom < threshold {
+                    break;
+                }
             }
-            if oom < threshold {
-                break;
+            if progressed {
+                println!("{}", states.total_prob);
+                last_total_prob = states.total_prob;
             }
-        }
-        if progressed {
-            println!("{}", states.total_prob);
-            last_total_prob = states.total_prob;
         }
     }
     let mut expected_cost = 0.;
     for (c, p) in states.successes.iter() {
-        let p: f32 = p.to_num(); //p.into();
-        expected_cost += p*(*c as f32);
+        let p: f64 = p.to_num(); //p.into();
+        expected_cost += p*(*c as f64);
     }
-    println!("Expected cost: {}", expected_cost * UNIT as f32);
+    println!("Expected cost: {}", expected_cost * UNIT as f64);
     println!("States pushed: {}, Merges: {}", states.push_counter, states.merge_counter);
     println!("Unmerged states pushed: {}", states.push_counter - states.merge_counter);
     println!("Smallest prob path: {}", states.min_prob);
